d329b73eb94255e8e91d06a21b34c082779e954f
chenyinheng30 <chenyinheng30@outlook.com>
2024-07-04 17:46:59 +0800

    Complete basic page design

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,191 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { Vue3NextQrcode } from 'vue3-next-qrcode'
+
+let LongUrl = ref("")
+let ShortUrl = ref("")
+let LongUrlInputMsg = ref("")
+let ShortUrlInputMsg = ref("")
+let QrcodeText = ref("https://tenet.chat")
+let RetentionDays = ref(7)
+let RetentionHours = ref(0)
+let RetentionMinutes = ref(0)
+
+function getUpperLetter(num: number) {
+  return String.fromCharCode(64 + num);
+}
+
+function getLowerLetter(num: number) {
+  return String.fromCharCode(96 + num);
+}
+
+function GetShortUrlPath(_Url: string) {
+  var array = new Uint16Array(6);
+  var path = "";
+  window.crypto.getRandomValues(array);
+  for (var i = 0; i < array.length; i++) {
+    var tok = array[i] % 62;
+    if (tok < 10) {
+      path += tok;
+    } else if (tok < 36) {
+      path += getUpperLetter(tok - 9);
+    } else {
+      path += getLowerLetter(tok - 35);
+    }
+  }
+  return path;
+}
+
+function GetLongUrlPath(_Url: string) {
+  return "https://example.com"
+}
+
+function SetLongUrl(event: any) {
+  LongUrlInputMsg.value = "";
+  var InputValue = event.target.value;
+  LongUrl.value = InputValue;
+  if (InputValue != "") {
+    if (InputValue.length < 1000) {
+      var MatchUrl = /^(https|http):\/\/([A-z0-9]\.|[A-z0-9][A-z0-9-]*[A-z0-9]\.)+[A-z]{2,}(:[0-9]{1,5})?(\/.*)?$/;
+      if (MatchUrl.test(InputValue)) {
+        var Url = "https://tenet.chat/";
+        Url += GetShortUrlPath(InputValue);
+        ShortUrl.value = Url;
+        QrcodeText.value = Url;
+      } else {
+        LongUrlInputMsg.value = "URL格式不匹配";
+      }
+    } else {
+      LongUrlInputMsg.value = "URL长度需要小于1000";
+    }
+  }
+}
+
+function SetShortUrl(event: any) {
+  ShortUrlInputMsg.value = "";
+  let InputValue = event.target.value;
+  ShortUrl.value = InputValue;
+  if (InputValue != "") {
+    if (InputValue.length <= 25) {
+      var MatchUrl = /^https:\/\/tenet.chat\/[A-z0-9]{6}$/;
+      if (MatchUrl.test(InputValue)) {
+        var Url = GetLongUrlPath(InputValue);
+        LongUrl.value = Url;
+        QrcodeText.value = Url;
+        return;
+      }
+    }
+    ShortUrlInputMsg.value = "URL不正确";
+  }
+}
 </script>
 
 <template>
+  <p>
+    <input class="mono_wieght" type="text" placeholder="输入长链接" :value="LongUrl" @change="SetLongUrl">
+  <div class="error_msg">{{ LongUrlInputMsg }}</div>
+  </p>
+  <p>
+    <input class="mono_wieght" type="text" placeholder="输入短链接" :value="ShortUrl" @change="SetShortUrl">
+  <div class="error_msg">{{ ShortUrlInputMsg }}</div>
+  </p>
+  <p class="mono_wieght center_parent">
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <section class="row_section">
+      <section class="column_section box">
+        <p class="rectangle">
+        <section class="row_section little_rectangle">
+          <div>天</div>
+          <div>小时</div>
+          <div>分钟</div>
+        </section>
+        <section class="row_section little_rectangle">
+          <div>
+            <input class="select_time" type="text" />
+          </div>
+          <div>
+            <input class="select_time" type="text" />
+          </div>
+          <div>
+            <input class="select_time" type="text" />
+          </div>
+        </section>
+        </p>
+        <p class="rectangle">cloudflare浏览器验证</p>
+      </section>
+      <Vue3NextQrcode :text="QrcodeText" :size=200 />
+    </section>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  </p>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+input[type=text] {
+  height: 2rem;
+  font-size: large;
+  border: 2px solid #ccc;
+  border-radius: 8px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.mono_wieght {
+  width: 50rem;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.row_section {
+  display: flex;
+  flex-direction: row;
 }
-</style>
+
+.column_section {
+  display: flex;
+  flex-direction: column;
+}
+
+.center_parent {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.box {
+  width: 12rem;
+  height: 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.rectangle {
+  width: 12rem;
+  height: 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.error_msg {
+  color: red;
+  font-family: arial;
+  font-size: small;
+  text-align: left;
+}
+
+.select_time {
+  width: 2rem;
+  height: 1rem;
+}
+
+.little_rectangle {
+  width: 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.little_rectangle>div {
+  width: 33%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>: { target: { value: any; }; }: { target: { value: any; }; }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// import Vue3NextQrcode from 'vue3-next-qrcode'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+// app.component('Vue3NextQrcode', Vue3NextQrcode);
+app.mount('#app');
